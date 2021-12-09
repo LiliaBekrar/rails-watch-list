e1dab0ba68d7362d9a2722fcae424aa7207ccde1
Lilia Bekrar <liliarossi2504@gmail.com>
2021-12-09 14:45:57 +0100

    add controller(bookmarks,lists) + seed + index

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,40 @@
+class ListsController < ApplicationController
+  before_action :find_list, only: %i[show delete]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def delete
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
